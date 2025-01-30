51b3a0da64a69bdd9674d4dbdbc2f403ad36eef6
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-01-30 17:28:14 +0100

    tailwind-extend better export

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 
-import animation from './tailwind-extend/animation';
-import brightness from './tailwind-extend/brightness';
-import colors from './tailwind-extend/colors';
-import fontSize from './tailwind-extend/fontSize';
-import keyframes from './tailwind-extend/keyframes';
-import scale from './tailwind-extend/scale';
-import screens from './tailwind-extend/screens';
-import spacing from './tailwind-extend/spacing';
-import transitionDuration from './tailwind-extend/transitionDuration';
-import transitionProperty from './tailwind-extend/transitionProperty';
-import zIndex from './tailwind-extend/zIndex';
-
-// const plugin = require('tailwindcss/plugin');
+import { animation, brightness, colors, fontSize, keyframes, scale, screens, spacing, transitionDuration, transitionProperty, zIndex } from "./tailwind-extend";
 
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  plugins: [
-    // require('@tailwindcss/forms'),
-    // require('daisyui'),
-  ],
-  // daisyui: {
-  //   themes: ["garden", "black"],
-  //   darkTheme: "black",
-  // },
   theme: {
     extend: {
       animation: animation,
