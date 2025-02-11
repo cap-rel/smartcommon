1a4a2eef7514e6be6f27971acd4b2c1d14ebf817
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-02-11 11:15:19 +0100

    New 404 page

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,2 +1,6 @@
+export * from "./Navbar";
+export * from "./NavbarLink";
 export * from "./Sidebar";
+export * from "./SidebarLink";
 export * from "./Tabbar";
+export * from "./TabbarLink";
---
+++ src/components/pages/errors/Error404Page/index.jsx
@@ -0,0 +1,9 @@
+export const Error404Page = (props) => {
+  return (
+    <div className={`fixed inset-0 bg-smt`}>
+      <div className={`absolute-full-center text-smt text-5xl font-bold`}>
+        404
+      </div>
+    </div>
+  );
+};
