dbde10ba38f8c0da5a945a2fdd5edb89001b3487
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-02-20 22:03:21 +0100

    Fix change tags in array

--- src/components/item/FormItem/index.jsx
+++ src/components/item/FormItem/index.jsx
@@ -1,8 +1,8 @@
-import { Address, Audios, Boolean, Checkbox, Drawing, Duration, Editor, Files, GpsPoints, IconSelect, Input, MultiNumber, Photos, Radio, Range, Rating, Select, Signature, Stepper, Tags, Textarea, Videos } from "../../form";
+import { Address, Audios, Boolean, Checkbox, Drawing, Duration, Editor, Files, GpsPoints, IconSelect, Input, MultiNumber, Photos, Radio, Range, Rating, Select, Signature, Stepper, Array, Textarea, Videos } from "../../form";
 import { useMemo } from "react";
 
 export const FormItem = (props) => {
-  const Components = useMemo(() => ({ Address, Audios, Boolean, Checkbox, Drawing, Duration, Editor, Files, GpsPoints, IconSelect, Input, MultiNumber, Photos, Radio, Range, Rating, Select, Signature, Stepper, Tags, Textarea, Videos }), []);
+  const Components = useMemo(() => ({ Address, Audios, Boolean, Checkbox, Drawing, Duration, Editor, Files, GpsPoints, IconSelect, Input, MultiNumber, Photos, Radio, Range, Rating, Select, Signature, Stepper, Array, Textarea, Videos }), []);
 
   const { type } = props;
 
