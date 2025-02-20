f6213705df2839bd2e4d8471db2f6a049d5d4bfb
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-02-20 21:55:20 +0100

    add button (props.js and floating position is missing)

---
+++ src/components/others/Button/index.jsx
@@ -0,0 +1,57 @@
+import { twMerge } from "tailwind-merge";
+import { isEmpty } from "../../../globals/functions";
+import { Icon } from "../Icon";
+
+export const Button = ({
+    leftIcon = null,
+    rightIcon = null,
+    floatingPosition = null,
+    variant = {
+        classNames: {
+            leftIcon: null,
+            rightIcon: null,
+            button: null
+        }
+    },
+    children = null,
+    ...props
+}) => {
+    const { classNames } = variant;
+
+    const floatingPositionClass = () => {
+        switch (floatingPosition) {
+            case "top-left": return "";
+            case "top": return "";
+            case "top-right": return "";
+            case "right": return "";
+            case "bottom-right": return "";
+            case "bottom": return "";
+            case "bottom-left": return "";
+            case "left": return "";
+            case "center": return "";
+            default: return "";
+        }
+    }
+    return (
+        <button
+            className={twMerge(`row-v-center gap-2 cursor-pointer p-2 bg-primary text-white button-smt rounded-md text-base ${floatingPositionClass()}`, classNames.button)}
+            { ...props}
+        >
+            {!isEmpty(leftIcon) &&
+                <Icon
+                    library={leftIcon.library}
+                    name={leftIcon.name}
+                    className={twMerge("text-xl", classNames.leftIcon)}
+                />
+            }
+            {children}
+            {!isEmpty(rightIcon) &&
+                <Icon
+                    library={rightIcon.library}
+                    name={rightIcon.name}
+                    className={twMerge("text-xl", classNames.rightIcon)}
+                />
+            }
+        </button>
+    );
+};
