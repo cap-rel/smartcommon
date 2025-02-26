a37f38be75062ea34543e2d04adb818a0105e10a
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-02-26 17:33:07 +0100

    New GpsPoints (not finished)

--- src/components/form/GpsPoints/index.jsx
+++ src/components/form/GpsPoints/index.jsx
@@ -1,40 +1,71 @@
 import toast from "react-hot-toast";
-import { getUserLocation } from "../../../globals/functions";
+import { getUserLocation, isEmpty } from "../../../globals/functions";
 import { Label } from "../Label";
 import { Icon } from "../../others";
 import { propTypes } from "./props";
+import { twMerge } from "tailwind-merge";
+import { useStates } from "../../../hooks";
 
 export const GpsPoints = ({
-  label = null,
-  id = null,
-  help = null,
-  readOnly = false,
-  required = false,
-  disabled = false,
-  value,
-  onChange = () => {},
-  color = null,
-  className = null
+  label,
+  labelRow = false,
+  help,
+  multiple = false,
+  onLocate,
+
+  containerProps,
+  labelContainerProps,
+  labelProps,
+  requiredStarProps,
+  helpProps,
+  inputProps,
+  ...props
 }) => {
-  const labelProps = { id, label, required, help, className };
-  const textareaProps = { id, required, disabled };
+  const GpsPointsPs = { ...props, inputProps };
+  const { disabled, required, readOnly, id, value, defaultValue } = GpsPointsPs
+
+  const { states, set } = useStates({
+    points: value ?? defaultValue ?? [0, 0],
+  });
+
+  const { points } = states;
+
+  const GspPointsPsForLabel = { disabled, required, readOnly, id };
+  const allLabelPs = { label, labelRow, help, containerProps, labelContainerProps, labelProps, requiredStarProps, helpProps, ...GspPointsPsForLabel };  
 
   return (
-    <Label { ...labelProps}>
+    <Label { ...allLabelPs}>
+      <input
+        { ...GpsPointsPs}
+        value={points[0]}
+        className={twMerge(`hidden`, GpsPointsPs?.className)}
+      />
+      <input
+        { ...GpsPointsPs}
+        value={points[1]}
+        className={twMerge(`hidden`, GpsPointsPs?.className)}
+      />
       <button 
         onClick={e => 
-          getUserLocation()
-            .then(location => {
-              console.log(`Succés de la géolocalisation: ${location}`);
-              onChange(location);
-              toast.success("Succès de la géolocalisation");
-            })
-            .catch(error => {
-              console.error(error);
-              toast.error("Erreur lors de la géolocalisation\n" + error);
-            })
+          {
+            e.preventDefault();
+            set("points", [45, 46]);
+            if (!isEmpty(onLocate)) {
+              onLocate([45, 46]);
+            }
+            // getUserLocation()
+            //   .then(location => {
+            //     console.log(`Succés de la géolocalisation: ${location}`);
+            //     onChange(location);
+            //     toast.success("Succès de la géolocalisation");
+            //   })
+            //   .catch(error => {
+            //     console.error(error);
+            //     toast.error("Erreur lors de la géolocalisation\n" + error);
+            //   })
+          }
         }
-        className={`p-4 border border-primary rounded-full bg-primary dark:bg-primary-20 text-white dark:text-primary text-2xl button-smt`}
+        className={`p-4 text-2xl text-white rounded-full border border-primary bg-primary dark:bg-primary-20 dark:text-primary button-smt`}
       >
         <Icon library={`fa6`} name={`FaLocationDot`} />
       </button>
