c15b1f7510bd24b1de7929ca429b37c4aa57469d
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-03-10 21:07:35 +0100

    Remove Router (no need because there is one page only (DevPage))

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -2,4 +2,3 @@ export * from "./react-helmet/Head";
 export * from "./react-i18next/I18nextProvider";
 export * from "./react-hot-toast/Toaster";
 export * from "./react-redux/ReduxProvider";
-export * from "./react-router-dom/Router";
