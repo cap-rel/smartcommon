7b1d08c6a465a80fea13bbaf2e2360fc276b5bb5
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-03-10 21:43:26 +0100

    Remove comments

--- src/components/index.js
+++ src/components/index.js
@@ -1,35 +1,9 @@
 export * from "./app";
 export * from "./form";
 export * from "./item";
-export * from "./layouts";
 export * from "./navigation";
 export * from "./others";
-// export * from "./pages";
-
-// Boolean
-    // star
-    // heart
-    // radio
-    // checkbox
-    // switch
-
-// OneSelection
-    // star
-    // heart
-    // radio
-    // select
-    // checkbox
-    // switch
-    // button
-
-// MultiSelection
-    // star
-    // heart
-    // radio
-    // select
-    // checkbox
-    // switch
-    // button
+export * from "./pages";
 
 // classes
     // divider => hr avec un text à l'interieur au début, au milieu, à la fin
@@ -42,62 +16,5 @@ export * from "./others";
     // avatar, mask => rounded, square, etc
     // skeleton
     // tooltip to replace title native prop ?
-
-// Refaire l'editeur pour que ce soit beaucoup plus simple ainsi que le drawing
-
-// opt group for select
-
-// FORM
-    // Address !
-    // audios !
-    // boolean ?
-    // multi ?
-    // single ?
-    // color => Voir si on ne peut pas utiliser l'input color natif
-    // drawing => à faire
-    // duration => voir si on n'utilise pas un input time ou si on utilise une librairie
-    // editor => refaire l'editor en mode simple: peut-être utiliser celui de material
-    // files => à faire
-    // gpsPoints => à faire
-    // iconSelect => voir si on ne peut pas le faire avec un select
-    // input => replacer les icons peut-être plus le outline à la place de la ring
-    // label ?
-    // multinumber => pour le double
-    // photos !
-    // range => vérifier si il fonctionne sur les autres navigateurs et rajourter le pourcentage
-    // rating => à finir
-    // Signature => à finir
-    // stepper => refaire le stepper avec les boutons sur la droite ou sur la gauche
-    // array => à refaire car pas très beau
-    // textarea => voir si on ne peut pas rajouter le mode note ou le mettre en grand écran
-    // videos !
-// LAYOUTS ? peut-être à supprimer
-    // publiclayout => à finir
-    // protectedlayout => à finir
-    // errorlayout => à faire
-    // ListLayout => à faire
-    //
-// NAVIGATION
-    // tabbar => à finir et proposer plus de choix
-    // sidebar => à finir et proposer plus de choix
-    // navbar
-// PAGES
-    // CodeError => à modifier pour qu'elle puisse prendre tous les codes possibles
-    // DevError => faire une page pour les outils en développement
-    // Login/Register/Forgot-Password/New-Password => on peut peut-être les faire
-// OTHERS
-    // Icon !
-    // LazyLink => à finir
-    // Spinner ? classe
-    // Chart
-    // Data table
-    // Calendar
-    // Carousel
-    // Accordion ? classe
-    // Popup ? classe
-    // Tooltip/Summary ? Classe
-    // stepper wiith multiple page ?
-    // Pagination
-    // List
 
 
