b6baca827e130fba2b000d546a2d57c855c49892
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-03-10 21:35:23 +0100

    Add DevPage (Styling component page)

---
+++ src/components/pages/DevPage/index.jsx
@@ -0,0 +1,61 @@
+import { Outlet } from "react-router-dom";
+import { Navbar, Sidebar, SidebarLink, Tabbar, TabbarLink } from "../../navigation";
+
+export const DevPage = ({ children }) => {
+    return (
+        <div id="main" className="min-h-screen text-strong-text bg-soft">
+            <div className={`relative mb-20 h-300`}>
+                {/* <Navbar
+                    title={`Accueil`}
+                /> */}
+                <Navbar title={`Accueil`}/>
+                {/* <Outlet /> */}
+                {children}
+                {/* <Sidebar>
+                    <SidebarLink
+                        label={`Accueil`}
+                        to={`/`}
+                        icon={{ library: "io5", name: "IoHome" }}
+                    />
+                    <SidebarLink
+                        label={`Carnet`}
+                        to={`/notes`}
+                        icon={{ library: "fa", name: "FaBook" }}
+                    />
+                    <SidebarLink
+                        label={`Synchroniser`}
+                        to={`/sync`}
+                        icon={{ library: "fa", name: "FaSyncAlt" }}
+                    />
+                    <SidebarLink
+                        label={`Paramètres`}
+                        to={`/settings`}
+                        icon={{ library: "fa6", name: "FaGear" }}
+                    />
+                </Sidebar> */}
+                <Tabbar>
+                    <TabbarLink
+                        label={`Accueil`}
+                        to={`/`}
+                        icon={{ library: "io5", name: "IoHome" }}
+                    />
+                    <TabbarLink
+                        label={`Carnet`}
+                        to={`/notes`}
+                        icon={{ library: "fa", name: "FaBook" }}
+                    />
+                    <TabbarLink
+                        label={`Synchroniser`}
+                        to={`/sync`}
+                        icon={{ library: "fa", name: "FaSyncAlt" }}
+                    />
+                    <TabbarLink
+                        label={`Paramètres`}
+                        to={`/settings`}
+                        icon={{ library: "fa6", name: "FaGear" }}
+                    />
+                </Tabbar>
+            </div>
+        </div>
+    );
+};
