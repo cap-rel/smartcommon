a67d6ab3d9e0653c7bcfb472f17037a72987c2fe
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-03-10 16:14:33 +0100

    New Editor

--- src/components/form/Editor/index.jsx
+++ src/components/form/Editor/index.jsx
@@ -1,40 +1,141 @@
-import { useEffect, useState } from "react";
-import { isEmpty } from "../../../globals/functions";
-import { Label } from "../../form";
-import MDEditor, { commands } from '@uiw/react-md-editor';
+import { useEffect } from "react";
+import { isEmpty, isNil } from "../../../globals/functions";
+import { Label, Textarea } from "../../form";
+// import MDEditor, { commands } from '@uiw/react-md-editor';
 import { propTypes } from "./props";
+import { useStates } from "../../../hooks";
+import { twMerge } from "tailwind-merge";
+import { FaEye, FaMarkdown } from "react-icons/fa6";
+import { Button } from "../../others";
+import { marked } from "marked";
 
-// TODO Faire les pattern
-// TODO Faire le required
-// TODO Faire le disabled (géré par mdeditor directement => à regarder)
+// TODO Style view (h1, h2, ...)
+// TODO Create a random id for textarea
 
 export const Editor = ({
-  label = null,
-  id = null,
-  help = null,
-  placeholder = null,
-  min = 0,
-  size = null,
-  max = null,
-  pattern = null,
-  rows = 5,
-  settings = null,
-  readOnly = false,
-  required = false,
-  disabled = false,
-  value,
-  onChange = () => {},
-  color = null,
-  className = null
-}) => {
-  const labelProps = { id, label, required, help, className };
-  const editorProps = { id, placeholder, required, disabled };
+  label,
+  labelRow = false,
+  help,
+  onValueChange = () => {},
 
-const [editorValue, setEditorValue] = useState(value);
-     
-useEffect(() => {
-  onChange(editorValue);
-}, [editorValue]);
+  containerProps,
+  labelContainerProps,
+  labelProps,
+  requiredStarProps,
+  helpProps,
+  textareaContainerProps,
+  textareaProps,
+  htmlProps,
+  buttonContainerProps,
+  mdButtonProps,
+  mdButtonIconProps,
+  mdButtonLabelProps,
+  htmlButtonProps,
+  htmlButtonIconProps,
+  htmlButtonLabelProps,
+  ...props
+}) => {
+  const textareaPs = { ...props, ...textareaProps };
+  const { required, readOnly, disabled, id, value, defaultValue } = textareaPs;
+
+  const textareaPsForLabel = { disabled, required, readOnly, id };
+
+  const allLabelPs = { label, labelRow, help, containerProps, labelContainerProps, labelProps, requiredStarProps, helpProps, ...textareaPsForLabel };
+
+  const { states, set } = useStates({
+    localValue: defaultValue ?? "",
+    isViewMode: false
+  });
+
+  const { localValue, isViewMode } = states;
+
+  const realValue = value ?? localValue;
+
+  const handleTextareaOnChange = (newValue) => {
+    if (isNil(value)) {
+      set("localValue", newValue);
+    } else {
+      onValueChange(newValue);
+    }
+  };
+
+  const switchToMarkDown = (e) => {
+    e.preventDefault();
+    set("isViewMode", false);
+  };
+
+  const switchToView = (e) => {
+    e.preventDefault();
+    set("isViewMode", true);
+  };
+
+  return (
+    <Label { ...allLabelPs}>
+      <div
+        { ...textareaContainerProps} 
+        className={twMerge(`rounded-md col`, textareaContainerProps?.className)}
+      >
+        <Textarea
+          placeholder={`${label}...` ?? ""}
+          { ...textareaPs}
+          id={`textarea`}
+          onValueChange={handleTextareaOnChange}
+          value={realValue}
+          className={twMerge(`rounded-b-none border-b-0 focus:ring-0 ${isViewMode && "hidden"}`, textareaPs?.className)}
+        />
+        <div
+          { ...htmlProps}
+          dangerouslySetInnerHTML={{ __html: marked(realValue) }}
+          style={{ "--height": `${document.querySelector("textarea")?.getBoundingClientRect().height}px`, ...htmlProps?.style }}
+          className={twMerge(`p-2 rounded-t-md border border-b-0 bg-strong border-soft-border overflow-y-auto h-(--height) ${!isViewMode && "hidden"}`, htmlProps?.className)}
+        />
+        <div
+          { ...buttonContainerProps}
+          className={twMerge(`row-v-center rounded-b-md rounded-t-none`, buttonContainerProps?.className)}
+        >
+          <Button
+            { ...mdButtonProps}
+            disabled={!isViewMode}
+            onClick={switchToMarkDown}
+            className={twMerge(`flex-1 p-2 rounded-none rounded-bl-md col-h-center gap-1 rounded-tl-none`, mdButtonProps?.className)}
+          >
+            <FaMarkdown
+              { ...mdButtonIconProps}
+              className={twMerge(`text-3xl`, mdButtonIconProps?.className)}
+            />
+            <div
+              { ...mdButtonLabelProps}
+              className={twMerge(`italic font-semibold`, mdButtonLabelProps?.className)}
+            >
+              Markdown
+            </div>
+          </Button>
+          <Button
+            { ...htmlButtonProps}
+            disabled={isViewMode}
+            onClick={switchToView}
+            className={twMerge(`flex-1 p-2 rounded-none rounded-br-md bg-secondary col-full-center gap-1 rounded-tr-none`, htmlButtonProps?.className)}
+          >
+            <FaEye
+              { ...htmlButtonIconProps}
+              className={twMerge(`text-3xl`, htmlButtonIconProps?.className)}
+            />
+            <div
+              { ...htmlButtonLabelProps}
+              className={twMerge(`italic font-semibold`, htmlButtonLabelProps?.className)}
+            >
+              Rendu
+            </div>
+          </Button>
+        </div>
+      </div>
+    </Label>
+  );
+
+
+
+
+// REACT MD EDITOR FOR DESKTOP
 
 // const toolbarCommands = [
 //   commands.bold,
@@ -48,48 +149,48 @@ useEffect(() => {
 //   commands.orderedListCommand,
 // ];
 
-return (
-  <Label { ...labelProps}>
-      <div className={`col w-full`}>
-        <MDEditor
-          className={`w-full`}
-          value={value}
-          onChange={setEditorValue}
-          textareaProps={{
-            placeholder: placeholder,
-            maxLength: maxLength
-          }}
-          preview={`edit`}
-          { ...editorProps}
-          // commandsFilter={(cmd) => {
-          //   if (/(live|preview|edit)/.test(cmd.name)) {
-          //     return false;
-          //   }
-          //   return cmd;
-          // }}
+// return (
+//   <Label { ...labelProps}>
+//       <div className={`w-full col`}>
+//         <MDEditor
+//           className={`w-full`}
+//           value={value}
+//           onChange={setEditorValue}
+//           textareaProps={{
+//             placeholder: placeholder,
+//             // maxLength: maxLength
+//           }}
+//           preview={`edit`}
+//           { ...editorProps}
+//           // commandsFilter={(cmd) => {
+//           //   if (/(live|preview|edit)/.test(cmd.name)) {
+//           //     return false;
+//           //   }
+//           //   return cmd;
+//           // }}
 
-          // excludeCommands={[
-          //   commands.codeLive,
-          //   commands.codePreview,
-          //   commands.image,
-          // ]}
+//           // excludeCommands={[
+//           //   commands.codeLive,
+//           //   commands.codePreview,
+//           //   commands.image,
+//           // ]}
 
-          // TODO Il faut probablement sécuriser l'éditeur un peu comme tous les autres champs
-          // previewOptions={{
-          //   rehypePlugins: [[rehypeSanitize]],
-          // }}
-        />
-        {/* <MDEditor.Markdown 
-          className="p-2 w-full text-xs"
-          source={
-            value 
-            ? `**Rendu :**  
-            ${value}` 
-            : "**Aucun rendu ...**"} 
-        /> */}
-      </div>
-    </Label>
-  )
+//           // TODO Il faut probablement sécuriser l'éditeur un peu comme tous les autres champs
+//           // previewOptions={{
+//           //   rehypePlugins: [[rehypeSanitize]],
+//           // }}
+//         />
+//         {/* <MDEditor.Markdown 
+//           className="p-2 w-full text-xs"
+//           source={
+//             value 
+//             ? `**Rendu :**  
+//             ${value}` 
+//             : "**Aucun rendu ...**"} 
+//         /> */}
+//       </div>
+//     </Label>
+//   )
 };
 
 Editor.propTypes = propTypes;
---
+++ src/components/form/Editor/props.js
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+
+export const propTypes = {
+    label: PropTypes.string,
+    labelRow: PropTypes.bool,
+    help: PropTypes.string,
+    onValueChange: PropTypes.func,
+  
+    containerProps: PropTypes.object,
+    labelContainerProps: PropTypes.object,
+    labelProps: PropTypes.object,
+    requiredStarProps: PropTypes.object,
+    helpProps: PropTypes.object,
+    textareaContainerProps: PropTypes.object,
+    textareaProps: PropTypes.object,
+    htmlProps: PropTypes.object,
+    buttonContainerProps: PropTypes.object,
+    mdButtonProps: PropTypes.object,
+    mdButtonIconProps: PropTypes.object,
+    mdButtonLabelProps: PropTypes.object,
+    htmlButtonProps: PropTypes.object,
+    htmlButtonIconProps: PropTypes.object,
+    htmlButtonLabelProps: PropTypes.object,
+};
