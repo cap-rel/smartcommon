3d355bd542c1a60290bb5be46b02316cdbe41ea2
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-04-18 13:41:13 +0200

    New Popup

--- src/components/others/Popup/index.jsx
+++ src/components/others/Popup/index.jsx
@@ -1,72 +1,24 @@
 import { Overlay } from "../Overlay";
 import { useVariantToProps } from "../../../hooks";
 import { propTypes } from "./props";
-// TODO Fix closeOnMove system
-
-// {
-//     overlay = true,
-//     shadow = false,
-//     closeOnClickOverlay = true,
-//     closeOnMove = false,
-//     isOpen = false,
-//     closePanel = () => {},
-//     floating = false,
-//     position = "left",
-//     overlayProps,
-//     panelProps,
-//     iconProps,
-//     ...props
-// }
+import { Button } from "../Button";
+import { RiCloseLargeLine } from "react-icons/ri";
+import { isNil } from "../../../globals";
 
 export const Popup = (props) => {
     const { variantProps, mergeProps } = useVariantToProps("popup", props);
 
     const { 
+        id,
         children,
         overlay = true,
         closeOnClickOverlay = true,
-        closeOnMove,
+        closeButton,
+        title,
         close,
         isOpen,
     } = variantProps;
     
-    // const { states, set } = useStates({
-    //     startTouch: null,
-    //     moveTouch: null
-    // })
-
-    // const { startTouch, moveTouch } = states;
-
-    // const moveMin = 30;
-    
-    // let positionClass;
-    // let icon;
-    // let cannotClose;
-    // let client;
-
-    // switch (position) {
-    //     case "top"   : positionClass = `${isOpen ? "top-(--initial)" : "-top-(--translate) point-events-none"} ${floating ? "left-2 right-2 rounded-xl" : "left-0 right-0 rounded-b-xl"} fixed pb-10`;
-    //                    icon = "absolute-h-center bottom-4 w-8 h-2";
-    //                    cannotClose = moveTouch + moveMin < startTouch;
-    //                    client = "clientY";
-    //                    break;
-    //     case "right" : positionClass = `${isOpen ? "right-(--initial)" : "-right-(--translate) point-events-none"} ${floating ? "top-2 bottom-2 rounded-xl" : "top-0 bottom-0 rounded-l-xl"} fixed pl-10`;
-    //                    icon = "absolute-v-center left-4 h-8 w-2";
-    //                    cannotClose = moveTouch - moveMin > startTouch;
-    //                    client = "clientX";
-    //                    break;
-    //     case "bottom": positionClass = `${isOpen ? "bottom-(--initial)" : "-bottom-(--translate) point-events-none"} ${floating ? "left-2 right-2 rounded-xl" : "left-0 right-0 rounded-t-xl"} fixed pt-10`;
-    //                    icon = "absolute-h-center top-4 w-8 h-2";
-    //                    cannotClose = moveTouch - moveMin > startTouch;
-    //                    client = "clientY";
-    //                    break;
-    //     default      : positionClass = `${isOpen ? "left-(--initial)" : "-left-(--translate) point-events-none"} ${floating ? "top-2 bottom-2 rounded-xl" : "top-0 bottom-0 rounded-r-xl"} fixed pr-10`;
-    //                    icon = "absolute-v-center right-4 h-8 w-2";
-    //                    cannotClose = moveTouch + moveMin < startTouch;
-    //                    client = "clientX";
-    //                    break;
-    // }
-   
     return (
         <>
             {overlay &&
@@ -76,52 +28,49 @@ export const Popup = (props) => {
                     close: closeOnClickOverlay && close
                 }))} />
             }
-            <div { ...mergeProps("popup", props => ({
+            <div { ...mergeProps("popupBackdrop", props => ({
                 ...props,
-                className: `rounded-app-lg fixed top-1/2 left-app-lg right-app-lg max-w-screen z-50 p-app-base gap-app-base flex flex-col duration-(--medium) bg-soft-bg ${isOpen ? "opacity-100" : "opacity-0 pointer-events-none"}`
+                className: `z-50 fixed inset-0 flex justify-center items-center p-app-lg duration-(--medium) pointer-events-none`
             }))}>
-                {/* <div 
-                    { ...iconProps}
-                    className={twMerge(`rounded-full bg-strong-border ${icon}`, iconProps?.className)}
-                /> */}
-                {children}
+
+                <div { ...mergeProps("popup", props => ({
+                    ...props,
+                    className: `w-full max-h-full rounded-app-md p-app-base overflow-y-auto gap-app-base text-app-base flex flex-col bg-soft-bg ${isOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}`
+                }))}>
+
+                    {(!isNil(title) || closeButton) &&
+                        <div { ...mergeProps("titleAndButtonContainer", props => ({
+                            ...props,
+                            className: `flex justify-between items-center gap-app-base`
+                        }))}>
+
+                            <div { ...mergeProps("title", props => ({
+                                ...props,
+                                className: `text-app-md font-app-semibold`
+                            }))}>
+                                {title}
+                            </div>
+
+                            {closeButton &&
+                                <Button { ...mergeProps("Button", props => ({
+                                    icon: <RiCloseLargeLine />,
+                                    ...props,
+                                    buttonProps: {
+                                        ...props.buttonProps,
+                                        onClick: close,
+                                        className: `text-app-lg z-60 bg-soft-bg text-soft-text p-app-xs rounded-app-xl -mr-app-xs -mt-app-xs`
+                                    },
+                                }))} />
+                            }
+                        </div>
+                    }
+
+                    {children}
+
+                </div>
             </div>
         </>
     );
 };
 
 Popup.prototypes = propTypes;
-
-{/* <Overlay 
-    { ...overlayProps}
-    isOpen={isOpen} 
-    closeOverlay={closeOnClickOverlay && closePanel} 
-/>
-<div
-    { ...panelPs}
-    style={{ 
-        "--duration": "300ms",
-        "--initial": floating ? "8px" : "0",
-        "--translate": floating ? "calc(100% + 8px)" : "100%",
-        ...panelPs?.style
-    }}
-    className={twMerge(`max-h-screen max-w-screen overflow-auto z-50 p-4 gap-4 col duration-(--duration) bg-strong ${positionClass}`, panelPs?.className)}
-    onTouchStart={e => {
-        set("startTouch", e.touches[0][client]);
-        set("moveTouch", e.touches[0][client]);
-    }}
-    onTouchMove={e => set("moveTouch", e.touches[0][client])}
-    onTouchEnd={e => {
-        if (closeOnMove && cannotClose) {
-            closePanel();
-        }
-        set("startTouch", null);
-        set("moveTouch", null);
-    }}
-    >
-    <div 
-        { ...iconProps}
-        className={twMerge(`rounded-full bg-strong-border ${icon}`, iconProps?.className)}
-    />
-    {children}
-</div> */}
