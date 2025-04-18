a6c7f8550a978e60a89a14ad94f302fda4f3e2e5
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-04-18 13:41:31 +0200

    Popup props

--- src/components/others/Popup/props.js
+++ src/components/others/Popup/props.js
@@ -1,12 +1,19 @@
 import PropTypes from "prop-types";
 
 export const propTypes = {
+    id: PropTypes.string,
     children: PropTypes.node,
     overlay: PropTypes.bool,
     closeOnClickOverlay: PropTypes.bool,
-    closeOnMove: PropTypes.bool,
+    title: PropTypes.string,
+    closeButton: PropTypes.bool,
     isOpen: PropTypes.bool,
     close: PropTypes.func,
+
     Overlay: PropTypes.object,
-    panelProps: PropTypes.object,
+    popupBackdrop: PropTypes.object,
+    popupProps: PropTypes.object,
+    titleAndButtonContainerProps: PropTypes.object,
+    titleProps: PropTypes.object,
+    Button: PropTypes.object
 }
