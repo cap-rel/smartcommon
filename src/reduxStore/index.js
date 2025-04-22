9164e82cb6253a5b31316c44b0b38ff24a2fbbfd
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-04-22 15:38:02 +0200

    redux index

--- src/reduxStore/index.js
+++ src/reduxStore/index.js
@@ -1,5 +1,9 @@
 import settingsReducer from "./reducers/settingsSlice";
-import authReducer from "./reducers/authSlice";
+import authReducer from "./reducers/userSlice";
+import configReducer from "./reducers/configSlice";
+import draftsReducer from "./reducers/draftsSlice";
+import updatesReducer from "./reducers/draftsSlice";
+import interventionsReducer from "./reducers/interventionsSlice";
 
 import { combineReducers } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
@@ -7,6 +11,10 @@ import { configureStore } from "@reduxjs/toolkit";
 const rootReducer = combineReducers({
   auth: authReducer,
   settings: settingsReducer,
+  config: configReducer,
+  updates: updatesReducer,
+  drafts: draftsReducer,
+  interventions: interventionsReducer
 });
 
 export const store = configureStore({ reducer: rootReducer });
