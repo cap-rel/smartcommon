75ed8f78aedab20c742e98554564d004a0d3104e
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-04-22 15:37:42 +0200

    settingsSlice

--- src/reduxStore/reducers/settingsSlice.js
+++ src/reduxStore/reducers/settingsSlice.js
@@ -1,18 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { getLocalJSON, setLocalJSON } from "../../globals/functions/storage";
+import { getLocalJSON, removeLocal, setLocalJSON } from "../../globals/functions/storage";
 
-const initialState = {
-  language: getLocalJSON("language") || navigator.languages[0],
+const initialState = getLocalJSON("settings") ?? {
+
+};
+  // language: getLocalJSON("language") || navigator.languages[0],
   // devise
   // système horraire
   //
   //
-};
 
 const settingsSlice = createSlice({
   name: "settings",
   initialState,
   reducers: {
+    setSettings(state, action) {
+      const settings = action.payload;
+      state = settings;
+      setLocalJSON("settings", settings);
+    },
+    unsetSettings(state) {
+      state = null;
+      removeLocal("settings")
+    },
     changeLanguage(state, action) {
       state.language = action.payload;
       setLocalJSON("language", action.payload);
@@ -21,4 +31,4 @@ const settingsSlice = createSlice({
 });
 
 export default settingsSlice.reducer;
-export const { changeLanguage } = settingsSlice.actions;
+export const { setSettings, unsetSettings, changeLanguage } = settingsSlice.actions;
