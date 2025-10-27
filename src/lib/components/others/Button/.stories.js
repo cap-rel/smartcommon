32f05ee61e0e0c040f2bd22e6579beddadb3cac4
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-10-27 19:17:07 +0100

    tests stories

--- src/lib/components/others/Button/.stories.js
+++ src/lib/components/others/Button/.stories.js
@@ -4,6 +4,7 @@ import { fn } from 'storybook/test';
 // import { Header } from "../Header/Header";
 
 import { Button } from './';
+import { FaApple, FaBook, FaUser } from 'react-icons/fa6';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 export default {
@@ -11,19 +12,30 @@ export default {
   component: Button,
   // subcomponents: { Header },
   parameters: {
+    // docs: {
+    //   source: {
+    //     transform: (src, { args }) => {
+    //        if (args.icon && typeof args.icon === "function") {
+    //         const iconName = args.icon.name;
+    //         return src.replace(/\(\) => {}/, iconName);
+    //       }
+    //       return src;
+    //     },
+    //   },
+    // },
     options: {
-      showPanel: false,
+      showPanel: true,
     },
     // docs: {
     //   codePanel: true
     // },
-    backgrounds: {
-      options: {
-        red: { name: 'Red', value: '#f00' },
-        green: { name: 'Green', value: '#0f0' },
-        blue: { name: 'Blue', value: '#00f' },
-      },
-    },
+    // backgrounds: {
+    //   options: {
+    //     red: { name: 'Red', value: '#f00' },
+    //     green: { name: 'Green', value: '#0f0' },
+    //     blue: { name: 'Blue', value: '#00f' },
+    //   },
+    // },
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
@@ -31,6 +43,57 @@ export default {
   tags: ["autodocs", "others"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
+    id: { 
+        control: false,
+        table: { category: 'Main' },
+    },
+    children: { 
+        control: false,
+        table: { category: 'Main' },
+    },
+    label: {
+        default: "Submit",
+        table: { category: 'Appearance' },
+    },
+    icon: { 
+        control: false,
+        table: { category: 'Appearance' },
+    },
+    badge: {
+        table: { category: 'Appearance' },
+    },
+    disabled: { 
+        table: { category: 'Status' },
+    },
+    loading: {
+        table: { category: 'Status' },
+    },
+    onClick: { 
+        table: { category: 'Events' },
+    },
+    buttonProps: { 
+        table: { category: 'Elements' },
+    },
+    Spinner: { 
+        table: { category: 'Elements' },
+    },
+    iconProps: { 
+        table: { category: 'Elements' },
+    },
+    labelProps: { 
+        table: { category: 'Elements' },
+    },
+    badgeProps: { 
+        table: { category: 'Elements' },
+    },
+    // icon: {
+    //     control: { type: "select" },
+    //     options: {
+    //         FaUser: FaUser, 
+    //         FaBook: FaBook, 
+    //         FaApple: FaApple
+    //     }
+    // }
     // backgroundColor: { control: 'color' },
     // label: {
     //   control: { type: 'select' },
@@ -43,11 +106,11 @@ export default {
     // },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
+//   args: { onClick: fn() },
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export { Primary } from "./stories";
+export { Default, Test } from "./stories";
 
 // export const Primary = {
 //     args: {}
