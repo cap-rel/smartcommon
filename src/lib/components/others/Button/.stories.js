b68b4753edfa9f6ee0cb1fef03fa0fafb03411cc
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-10-27 10:43:02 +0100

    add stories folder  (contains stories) f in components folder

--- src/lib/components/others/Button/.stories.js
+++ src/lib/components/others/Button/.stories.js
@@ -47,29 +47,8 @@ export default {
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary = {
-  args: {
-    // primary: true,
-    // label: 'Button',
-  },
-};
+export { Primary } from "./stories";
 
-// export const Secondary = {
-//   args: {
-//     label: 'Button',
-//   },
-// };
-
-// export const Large = {
-//   args: {
-//     size: 'large',
-//     label: 'Button',
-//   },
-// };
-
-// export const Small = {
-//   args: {
-//     size: 'small',
-//     label: 'Button',
-//   },
+// export const Primary = {
+//     args: {}
 // };
---
+++ src/lib/components/others/Button/stories/Primary.jsx
@@ -0,0 +1,6 @@
+export const Primary = {
+  args: {
+    // primary: true,
+    // label: 'Button',
+  },
+};
