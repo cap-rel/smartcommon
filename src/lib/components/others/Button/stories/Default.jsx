ecb7eee29d719d893c6807eebef06d6e9d6308f9
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-10-28 13:48:56 +0100

    update the button stories

--- src/lib/components/others/Button/stories/Default.jsx
+++ src/lib/components/others/Button/stories/Default.jsx
@@ -1,5 +1,24 @@
+import { FaUser } from "react-icons/fa6";
+
 export const Default = {
   args: {
-    label: "Submit",
+    label: "Login",
+    icon: FaUser
   },
+  parameters: {
+    docs: {
+      source: {
+        code: `
+          import { Button } from "@cap-rel/smartcommon";
+          import { FaUser } from "react-icons/fa6";
+
+          <Button
+            id="login-button"
+            label="Login"
+            icon={FaUser}
+          />
+        `
+      }
+    }
+  }
 };
--- src/lib/components/others/Button/stories/Test.jsx
+++ src/lib/components/others/Button/stories/Test.jsx
@@ -1,5 +1,6 @@
 export const Test = {
   args: {
+    label: "Test"
     // primary: true,
     // label: 'Button',
   },
