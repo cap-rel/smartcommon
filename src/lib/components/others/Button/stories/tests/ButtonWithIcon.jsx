35cb0055fef3472969e239b3ebc1d1878a211219
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-10-29 08:39:23 +0100

    Add ButtonWithIcon to test

---
+++ src/lib/components/others/Button/stories/tests/ButtonWithIcon.jsx
@@ -0,0 +1,17 @@
+import { FaUser } from "react-icons/fa6";
+
+export const ButtonWithIcon = {
+    name: "bonjour",
+    args: {
+        label: "Login",
+        icon: FaUser
+    },
+    argTypes: {
+        id: { table: { disable: true } }
+        // id: { table: { disable: true } }
+        // id: { table: { disable: true } }
+        // id: { table: { disable: true } }
+        // id: { table: { disable: true } }
+
+    }
+};
