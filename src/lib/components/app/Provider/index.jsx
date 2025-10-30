3536df9083b3dc42cfe346903ea1d6db3366614f
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-10-30 12:33:36 +0100

    comments on smartcommon provider

--- src/lib/components/app/Provider/index.jsx
+++ src/lib/components/app/Provider/index.jsx
@@ -1,60 +1,61 @@
 import { createContext } from "react";
+import { I18nextProvider } from "../I18nextProvider";
 
 export const LibContext = createContext();
 
 export const Provider = (props) => {
-  const { children, mergedClass, config, variants, themes, theme, i18n } = props;
+  const { children, mergedClass, config, variants, themes } = props;
   
   return (
-    <LibContext.Provider value={{ mergedClass, config, variants, themes, theme, i18n }}>
+    <LibContext.Provider value={{ config }}>
       {children}
     </LibContext.Provider>
   );
-};  
-  // const dispatch = useDispatch();
+};
 
-    // const initVariables = variables => {
-    //     Object.keys(variables ?? {}).forEach(key => html.style.removeProperty(key));
-    // };
+// TODO find an object to manage mergedClass
+// const mergedClass = {
+//   duration: ["test", "app", "app-xs"]
+// }
 
-    // const setVariables = variables => {
-    //     Object.entries(variables ?? {}).forEach(entries => html.style.setProperty(...entries));
-    // };
+// TODO voir la correspondance entre les themes, les variantes et les themes css
 
-    // useEffect(() => {
-    //     dispatch(saveSettings({ lastTheme: theme }));
+// TODO a voir si on met une variante dans une variante
 
-    //     const currentTheme = themes?.[theme] ?? {};
-    //     const lastTheme = themes?.[lastTh] ?? {};
+// Pour ignorer les variantes d'un theme => !t ou ["!t"]
+// Pour ignorer les variantes d'un theme => !t-variant ou ["!t-variant"]
+// Pour ignorer les variantes par défaut => "!d" ou ["!d"]
+// Pour ignorer une variante par défaut en particulier => "!d-variant" ou ["!d-variant"]
+const config = {
+  // Thème sélectionné
+  theme: "",
+  // Thèmes fournis par le dev => { theme1: {}, theme2: {} }
+  themes: {},
+  // Variantes fournis par le dev => { Button: { variant1, variant2 } }
+  variants: {},
+  // Configuration des composants (pour l'instant, il n'y a que les variantes (array, string ou object))
+  components: {
+    Button: {
+      variant: ""
+    },
+    Spinner: ""
+  },
+  // Classes qui doivent être prises en compte par le twMerge. Si une classe n'a pas l'air de fonctionner en dev, on la met la dedans
+  mergedClass: "",
+  // Par défaut en anglais, oui ou non smartcommon est traduit donc utilisation des function de i18next
+  translated: true
+};
 
-    //     if ("default" in lastTheme) {
-    //         initVariables(lastTheme.default?.variables);
-    //         initVariables(lastTheme.dark?.variables);
-    //     } else {
-    //         initVariables(lastTheme.variables);
-    //     }
+// Deux moyens de faire configurer les variantes des composants par défaut:
+// 
+// - Configurer par défaut
 
-    //     let current = "default";
-    //     let last = "dark";
+const useSC = () => {
+  return { themes, variants, SCVariantsOnly, SCThemesOnly }
+};
 
-    //     if (darkMode) {
-    //         current = "dark";
-    //         last = "default"
-    //         html.classList.add("dark");
-    //     } else {
-    //         html.classList.remove("dark");
-    //     }
+// i18n => download JSON files or copy JSON from the site
 
-    //     const currentVariables = currentTheme[current]?.variables;
-    //     const lastVariables = currentTheme[last]?.variables;
-
-    //     if ("default" in currentTheme) {
-    //         const variablesToInit = Object.fromEntries(Object.entries(lastVariables ?? {}).filter(([key, value]) => !Object.keys(currentVariables ?? {}).includes(key)));
-
-    //         setVariables(currentVariables);
-    //         initVariables(variablesToInit);
-    //     } else {
-    //         setVariables(currentTheme.variables);
-    //     }
-
-    // }, [theme, themes, darkMode]);
+// useVariantMerge => mergeProps, variantProps, mergeQuickProps, setParams ?
+// Provider
+// useConfig ou useSmartCommon
