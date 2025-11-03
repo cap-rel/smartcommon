cfe983982228d10798667bfdee7e8a361f2f6e8a
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-11-03 18:03:27 +0100

    Panel test in dev page

--- src/interface/components/pages/DevPage2/index.jsx
+++ src/interface/components/pages/DevPage2/index.jsx
@@ -1,12 +1,27 @@
 import { Link, useLocation } from "react-router-dom";
-import { Page } from "../../../../lib";
+import { Page, Panel } from "../../../../lib";
+import { useState } from "react";
 
 export const DevPage2 = () => {
     const location = useLocation();
 
+    const [isPanelOpen, setIsPanelOpen] = useState(false);
+
+    console.log(isPanelOpen);
+
     return (
         <Page>
             <Link to={"/"}>Bonjour</Link>
+            <button onClick={() => setIsPanelOpen(true)}>Open</button>
+
+            <Panel
+                isOpen={isPanelOpen}
+                close={() => setIsPanelOpen(false)}
+            >
+                <div className="h-100">
+
+                </div>
+            </Panel>
         </Page>
     );
 };
