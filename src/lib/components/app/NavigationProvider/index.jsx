bfdbdd108bf5a9072b07ee81ffb64f6cd5456326
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-11-03 16:22:32 +0100

    add history management in navigation provider

--- src/lib/components/app/NavigationProvider/index.jsx
+++ src/lib/components/app/NavigationProvider/index.jsx
@@ -1,12 +1,39 @@
-import { createContext } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
+import { useLocation } from "react-router-dom";
 
-export const NavigationContext = createContext();
+export const NavigationContext = createContext({
+  history: [],
+  pathname: null,
+  prevPathname: null
+});
 
 export const NavigationProvider = (props) => {
   const { children } = props;
   
+  const location = useLocation() ?? {};
+  const { pathname } = location;
+
+  const [history, setHistory] = useState([]);
+  const locationNb = history.length;
+//   const prevPathRef = useRef(null);
+
+  useEffect(() => {
+    setHistory(prevHistory => {
+    //   if (prevHistory[prevHistory.length - 2] === location.pathname) {
+    //     return prevHistory.slice(0, -1);
+    //   }
+
+      // Sinon, ajouter le nouveau chemin
+      return [...prevHistory, location];
+    });
+
+    // prevPathRef.current = location.pathname;
+  }, [pathname]);
+
+  const prevPathname = locationNb > 1 ? history[locationNb - 2] : null;
+
   return (
-    <NavigationContext.Provider>
+    <NavigationContext.Provider value={{ history, pathname, prevPathname }}>
       {children}
     </NavigationContext.Provider>
   );
