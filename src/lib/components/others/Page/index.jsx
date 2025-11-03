9cbc57550d86621bdf5ee8cfcdb745485cab11f3
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-11-03 11:11:07 +0100

    Add animation system in Page component

--- src/lib/components/others/Page/index.jsx
+++ src/lib/components/others/Page/index.jsx
@@ -1,25 +1,116 @@
-import { useEffect } from "react";
-import { useStates, useVariantMerger } from "../../../hooks";
+import { usePreviousPathname, useVariantMerger } from "../../../hooks";
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+const config = {
+    animations: {
+        slideRight: {
+            initial: { x: "50%", opacity: 0 },
+            animate: { x: 0, opacity: 1, transition: { duration: 0.15, ease: "easeInOut" } },
+            exit: { x: "50%", opacity: 0, transition: { duration: 0.15, ease: "easeInOut" } },
+        },
+        slideLeft: {
+            initial: { x: "-50%", opacity: 0 },
+            animate: { x: 0, opacity: 1, transition: { duration: 0.15, ease: "easeInOut" } },
+            exit: { x: "-50%", opacity: 0, transition: { duration: 0.15, ease: "easeInOut" } },
+        },
+        fade: {
+            initial: { opacity: 0 },
+            animate: { opacity: 1, transition: { duration: 0.15, ease: "easeOut" } },
+            exit: { opacity: 0, transition: { duration: 0.15, ease: "easeOut" }},
+            // transition: { duration: 0.2, ease: "easeInOut" },
+        },
+        zoom : {
+            initial: { scale: 0.9, opacity: 0 },
+            animate: { scale: 1, opacity: 1 },
+            exit: { scale: 0.9, opacity: 0 },
+            transition: { duration: 0.2, ease: "easeOut" },
+        }
+    },
+    pages: {
+        "/": {
+            "/dev2": "slideLeft",
+            "*": "fade"
+        },
+        "/dev2": {
+            "/": "slideRight",
+            "*": "fade"
+        },
+        // "/": "fade",
+        "*": "fade"
+    }
+};
+
+let prevPathname = null;
 
 export const Page = (props) => {
     const { variantProps, mergeProps } = useVariantMerger("Page", props);
 
     const { children } = variantProps;
 
-    const { states, set } = useStates({
-        init: false
-    });
+    const location = useLocation();
 
-    const { init } = states;
+    useEffect(() => {
+        prevPathname = location.pathname;
+    }, [location.pathname]);
 
-    useEffect(() => set("init", true), []);
+    const { animations = {}, pages = {} } = config ?? {};
+
+    const animation = () => {
+        const page = pages[location?.pathname];
+        if (page) {
+        if (typeof page === "string") {
+            return page
+        } else {
+            const prevPage = page[prevPathname];
+            if (prevPage) {
+            return prevPage;
+            } else {
+            return page["*"];
+            }
+        }
+        } else {
+        const page = pages["*"];
+        if (typeof page === "string") {
+            return page
+        } else {
+            const prevPage = page[prevPathname];
+            if (prevPage) {
+            return prevPage;
+            } else {
+            return page["*"];
+            }
+        }
+        }
+    };
+
+//     const animation = () => {
+//     const page = pages[location?.pathname];
+//     if (page) {
+//       if (typeof page === "string") return page;
+//       return page[prevPathname] || page["*"];
+//     } else {
+//       const page = pages["*"];
+//       if (typeof page === "string") return page;
+//       return page[prevPathname] || page["*"];
+//     }
+//   };
 
     return (
-        <div { ...mergeProps("page", props => ({
-            ...props,
-            className: `fixed inset-0 bg-medium-bg overflow-y-auto text-strong-text text-app-sm duration-(--medium) ${init ? "opacity-100" : "opacity-0"}`
-        }))}>
-            {children}
-        </div>
+        <AnimatePresence mode="wait">
+            <motion.div
+                key={location.pathname} // important pour déclencher l'animation au changement de route
+                className="fixed inset-0"
+                { ...animations[animation()]}
+            >
+                <div { ...mergeProps("page", props => ({
+                    ...props,
+                    className: `fixed inset-0 bg-medium-bg overflow-y-auto text-strong-text text-app-sm`
+                }))}>
+                    {children}
+                </div>
+            </motion.div>
+        </AnimatePresence>
     );
-}
+};
