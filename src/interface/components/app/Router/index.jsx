55582bd8ed887eb76b93c8e1082042d384d16bd4
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-11-03 16:54:22 +0100

    Add navigationProvider in router to test

--- src/interface/components/app/Router/index.jsx
+++ src/interface/components/app/Router/index.jsx
@@ -2,17 +2,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { componentNames } from "../../pages/SmartPage/components";
 import { DevPage, SmartPage } from "../../pages";
 import { DevPage2 } from "../../pages/DevPage2";
+import { NavigationProvider } from "../../../../lib";
 
 export const Router = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path={`/`} element={<DevPage />}/>
-                <Route path={`/dev2`} element={<DevPage2 />}/>
-                {/* {componentNames.map((component, CI) =>
-                    <Route key={`component${CI}`} path={`/${component}`} element={<SmartPage />} />
-                )} */}
-            </Routes>
+            <NavigationProvider>
+                <Routes>
+                    <Route path={`/`} element={<DevPage />}/>
+                    <Route path={`/dev2`} element={<DevPage2 />}/>
+                    {/* {componentNames.map((component, CI) =>
+                        <Route key={`component${CI}`} path={`/${component}`} element={<SmartPage />} />
+                    )} */}
+                </Routes>
+            </NavigationProvider>
         </BrowserRouter>
     );
 };
