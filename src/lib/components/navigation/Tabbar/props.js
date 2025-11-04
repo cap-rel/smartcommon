e6d326bcdcf777936f7b6c7c8561b51b83a17dd1
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-11-04 17:43:10 +0100

    Add hideOnScroll prop for tabbar

--- src/lib/components/navigation/Tabbar/props.js
+++ src/lib/components/navigation/Tabbar/props.js
@@ -4,11 +4,16 @@ export const propTypes = {
     id: PropTypes.string,
     children: PropTypes.node,
     links: PropTypes.arrayOf(PropTypes.object),
-    centralButton: PropTypes.object, // ?
+    hideOnScroll: PropTypes.bool,
+    centralButton: PropTypes.object, // TODO
     tabbarProps: PropTypes.object,
     linkProps: PropTypes.object,
     iconAndLabelContainerProps: PropTypes.object,
     iconProps: PropTypes.object,
     badgeProps: PropTypes.object,
     labelProps: PropTypes.object,
+};
+
+export const defaultProps = {
+    hideOnScroll: false
 };
