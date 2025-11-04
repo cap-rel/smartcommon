eb63de00dacacef91a679b5e3053691d8575657b
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-11-04 18:54:32 +0100

    this goes with the last commit

--- src/interface/components/pages/DevPage2/index.jsx
+++ src/interface/components/pages/DevPage2/index.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-import { Page, Panel } from "../../../../lib";
+import { Page, Panel, Tabbar } from "../../../../lib";
 import { useState } from "react";
 
 export const DevPage2 = () => {
@@ -8,19 +8,27 @@ export const DevPage2 = () => {
     const [isPanelOpen, setIsPanelOpen] = useState(false);
 
     return (
-        <Page>
-            <Link to={"/"}>Bonjour</Link>
-            <button onClick={() => setIsPanelOpen(true)}>Open</button>
+        <>
+            <Page pageProps={{ id: "Principal" }}>
+                <div className="flex flex-col">
+                    <Link to={"/"}>Bonjour</Link>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
 
-            <Panel
-                position="bottom"
-                isOpen={isPanelOpen}
-                close={() => setIsPanelOpen(false)}
-            >
-                <div className="min-w-50 min-h-100">
+                    <Panel
+                        position="bottom"
+                        isOpen={isPanelOpen}
+                        close={() => setIsPanelOpen(false)}
+                    >
+                        <div className="min-w-50 min-h-100">
 
+                        </div>
+                    </Panel>
+                    <div className="h-[2000px] bg-orange-500">
+                        sfdsqfq
+                    </div>
                 </div>
-            </Panel>
-        </Page>
+            </Page>
+            <Tabbar />
+        </>
     );
 };
