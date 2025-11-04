7b29eec5273df2432b220fbb01c7b913fa5c840f
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-11-04 18:40:54 +0100

    add id prop to Page

--- src/lib/components/others/Page/props.js
+++ src/lib/components/others/Page/props.js
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 
 export const propTypes = {
+    id: PropTypes.string,
     animations: PropTypes.object,
     children: PropTypes.node,
     pageProps: PropTypes.object,
