222f73456f8b375de199bda82c2b3084d2b22e0a
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-11-05 11:53:28 +0100

    add apiProvider to the main provider

--- src/lib/components/app/Provider/index.jsx
+++ src/lib/components/app/Provider/index.jsx
@@ -1,17 +1,20 @@
 import { AnimatePresence } from "framer-motion";
 import { LibProvider } from "../LibProvider";
 import { NavigationProvider } from "../NavigationProvider";
+import { ApiProvider } from "../ApiProvider";
 
 export const Provider = (props) => {
   const { children, config } = props;
   // TODO voir à quoi sert réellement AnimatePresence car ça fonctionne sans
   return (
     <LibProvider value={{ config }}>
-      <NavigationProvider>
-        <AnimatePresence mode="wait">
-        {children}
-        </AnimatePresence>
-      </NavigationProvider>
+      {/* <NavigationProvider> */}
+        <ApiProvider>
+          <AnimatePresence mode="wait">
+          {children}
+          </AnimatePresence>
+        </ApiProvider>
+      {/* </NavigationProvider> */}
     </LibProvider>
   );
 };
