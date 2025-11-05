20af2316cc7cd46fa871f7da072b963029eb9499
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-11-05 17:20:29 +0100

    remove tabbar props

--- src/lib/components/navigation/Tabbar/props.js
+++ src/lib/components/navigation/Tabbar/props.js
@@ -1,17 +1,11 @@
 import PropTypes from "prop-types";
 
 export const propTypes = {
-    id: PropTypes.string,
+    id: PropTypes.string.isRequired,
     children: PropTypes.node,
-    links: PropTypes.arrayOf(PropTypes.object),
     hideOnScroll: PropTypes.bool,
     centralButton: PropTypes.object, // TODO
     tabbarProps: PropTypes.object,
-    linkProps: PropTypes.object,
-    iconAndLabelContainerProps: PropTypes.object,
-    iconProps: PropTypes.object,
-    badgeProps: PropTypes.object,
-    labelProps: PropTypes.object,
 };
 
 export const defaultProps = {
