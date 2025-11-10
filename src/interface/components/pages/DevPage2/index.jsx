4334939f06d4e8c92fe35a8515c854c08d66c886
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-11-10 22:55:44 +0100

    Add components in dev page

--- src/interface/components/pages/DevPage2/index.jsx
+++ src/interface/components/pages/DevPage2/index.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-import { Block, LowerNavbarItem, Navbar, Page, Panel, Tabbar, TabbarItem } from "../../../../lib";
+import { Block, List, LowerNavbarItem, Navbar, Page, Panel, Tabbar, TabbarItem } from "../../../../lib";
 import { useState } from "react";
 import { FaUserAstronaut, FaUser, FaGear, FaGears } from "react-icons/fa6";
 
@@ -30,37 +30,62 @@ export const DevPage2 = () => {
                         />
                     </>}
                 />
-                    <Block title={"bonjour"}>
-                        <Link to={"/"}>
-                            Bonjour
-                        </Link>
-                        <button onClick={() => setIsPanelOpen(true)}>Open</button>
-                    </Block>
-                     <Block title={"bonjour"}>
-                        <Link to={"/"}>
-                            Bonjour
-                        </Link>
-                        <button onClick={() => setIsPanelOpen(true)}>Open</button>
-                    </Block>
-                     <Block title={"bonjour"}>
-                        <Link to={"/"}>
-                            Bonjour
-                        </Link>
-                        <button onClick={() => setIsPanelOpen(true)}>Open</button>
-                    </Block>
+                <List />
+                <Block title={"Interventions à faire"}>
+                    <Link to={"/"}>
+                        Bonjour
+                    </Link>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
 
-                    {/* <Panel
-                        position="bottom"
-                        isOpen={isPanelOpen}
-                        close={() => setIsPanelOpen(false)}
-                    >
-                        <div className="min-w-50 min-h-100">
+                </Block>
+                <Block title={"Interventions urgentes"}>
+                    <Link to={"/"}>
+                        Bonjour
+                    </Link>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
 
-                        </div>
-                    </Panel>
-                    <div className="h-[2000px] flex flex-col justify-end">
-                        Fin de page
-                    </div> */}
+                </Block>
+                <Block title={"bonjour"}>
+                    <Link to={"/"}>
+                        Bonjour
+                    </Link>
+                    <button onClick={() => setIsPanelOpen(true)}>Open</button>
+                </Block>
+
+                {/* <Panel
+                    position="bottom"
+                    isOpen={isPanelOpen}
+                    close={() => setIsPanelOpen(false)}
+                >
+                    <div className="min-w-50 min-h-100">
+
+                    </div>
+                </Panel>
+                <div className="h-[2000px] flex flex-col justify-end">
+                    Fin de page
+                </div> */}
                 <Tabbar id={"tabbar"}>
                     <TabbarItem
                         icon={FaUser}
