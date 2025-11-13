b9d7e1e4e19b8aca3a86b1e8debc37eda924a2b9
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-11-13 17:16:26 +0100

    export locate

--- src/lib/utils/functions/others.js
+++ src/lib/utils/functions/others.js
@@ -235,3 +235,17 @@ export function mergeObj(obj1, obj2) {
   }
   return mergedObj;
 };
+
+export const locate = (success = () => {}, error = () => {}) => {
+    navigator.geolocation.getCurrentPosition(
+        position => {
+          const coords = [position.coords.latitude, position.coords.longitude];
+          console.log(`Geolocation success`, coords);
+          success(coords);
+        },
+        err => {
+          console.error(`Geolocation error`, err);
+          error(err);
+        }
+    )
+}
