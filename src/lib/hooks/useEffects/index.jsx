41d305b92fec3a94487c59e22d186db430e1d99a
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-12-03 11:45:10 +0100

    Change 0 to 1

--- src/lib/hooks/useEffects/index.jsx
+++ src/lib/hooks/useEffects/index.jsx
@@ -5,7 +5,7 @@ import { useStates } from "lib/hooks";
 import { log } from "lib/utils";
 
 export const useEffects = (effects) => {
-    const { states, set } = useStates(mapValues(effects, constant(0)));
+    const { states, set } = useStates(mapValues(effects, constant(1)));
 
     forEach(effects, ({ deps, effect = () => {} }, key) => {
         if (isFunction(effect)) {
