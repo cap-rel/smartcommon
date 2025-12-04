0575d4264458309e6f34191c3a2959f522c20a67
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-12-04 11:50:56 +0100

    useGlobalStates tests

--- src/dev/components/pages/DevPage3/index.jsx
+++ src/dev/components/pages/DevPage3/index.jsx
@@ -1,7 +1,8 @@
-import { useApi, useEffects, useStates } from "lib/hooks";
+import { useApi, useEffects, useGlobalStates, useStates } from "lib/hooks";
 import { useDispatch } from "react-redux";
 import { setUser } from "lib/global-state";
 import { useEffect } from "react";
+import { Page } from "lib/components";
 
 export const DevPage3 = () => {
     const { login } = useApi();
@@ -19,20 +20,16 @@ export const DevPage3 = () => {
         isLoggingIn: false
     };
 
-    const { states, set, unset, get } = useStates({ initialStates });
-
-    const { isLoggingIn } = states;
+    const { states, set, unset, get } = useStates({ initialStates, debug: true });
+    
+    const { globalStates, setGlobal, unsetGlobal, getGlobal } = useGlobalStates({ debug: true })
 
     useEffects({
         statesChange: {
             deps: states,
             effect: () => console.log(states)
         }
-    })
-
-    // useEffect(() => {
-    //     console.log(states);
-    // }, [states]);
+    });
 
     const test = () => {
         login({ email: "jeangeorges", password: "MoN4fPrxUbU5", rememberMe: true })
@@ -45,11 +42,13 @@ export const DevPage3 = () => {
     };
 
     return (
-        <div className="flex flex-col gap-app-base">
-            <button onClick={() => set("email", "paolo")}>email test</button>
-            <button onClick={() => set("loginData.email", "paolo")}>email</button>
-            <button onClick={() => set("loginData.test[3].test", { test: "bonjour" })}>mot de passe</button>
-            <button onClick={() => test()}>connexion</button>
-        </div>
+        <Page id="login-page">
+            <div className="flex flex-col gap-app-base">
+                <button onClick={() => set("email", "paolo")}>email test</button>
+                <button onClick={() => set("loginData.entities.test", "paolo")}>email</button>
+                <button onClick={() => console.log(getGlobal("loginData.bonjour"))}>mot de passe</button>
+                <button onClick={() => setGlobal("loginData.bonjour", "paola", "session")}>connexion</button>
+            </div>
+        </Page>
     );
 };
