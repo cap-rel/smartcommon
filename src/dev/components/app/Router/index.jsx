a8c2c4fdd87352d6e24fd35fdc7751b5f3b8a5f9
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-12-04 15:03:25 +0100

    reorganize dependencies

--- src/dev/components/app/Router/index.jsx
+++ src/dev/components/app/Router/index.jsx
@@ -1,21 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { componentNames } from "../../pages/SmartPage/components";
 import { DevPage, DevPage3, SmartPage } from "../../pages";
 import { DevPage2 } from "../../pages/DevPage2";
 
 export const Router = () => {    
-    return (
-        <BrowserRouter>
-            {/* <NavigationProvider> */}
-                <Routes>
-                    <Route path={`/`} element={<DevPage />}/>
-                    <Route path={`/dev2`} element={<DevPage2 />}/>
-                    <Route path={`/dev3`} element={<DevPage3 />}/>
-                    {/* {componentNames.map((component, CI) =>
-                        <Route key={`component${CI}`} path={`/${component}`} element={<SmartPage />} />
-                    )} */}
-                </Routes>
-            {/* </NavigationProvider> */}
-        </BrowserRouter>
+    return (""
+        // <BrowserRouter>
+        //     {/* <NavigationProvider> */}
+        //         <Routes>
+        //             <Route path={`/`} element={<DevPage />}/>
+        //             <Route path={`/dev2`} element={<DevPage2 />}/>
+        //             <Route path={`/dev3`} element={<DevPage3 />}/>
+        //             {/* {componentNames.map((component, CI) =>
+        //                 <Route key={`component${CI}`} path={`/${component}`} element={<SmartPage />} />
+        //             )} */}
+        //         </Routes>
+        //     {/* </NavigationProvider> */}
+        // </BrowserRouter>
     );
 };
