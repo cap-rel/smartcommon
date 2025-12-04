73ffdec3f024f5dcc30832d363f6cb516598999d
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-12-04 16:45:39 +0100

    remove test

--- src/lib/components/form/Input/index.jsx
+++ src/lib/components/form/Input/index.jsx
@@ -6,8 +6,6 @@ import { applyFunctionIfNotNil, datetimeFormat, isEmpty, isNil, isNumber, timeTo
 import { Label } from "lib/components";
 import { useStates, useLocalValue, useVariantMerger } from "lib/hooks";
 
-import { useLocation } from "react-router-dom";
-
 import { defaultProps, propTypes } from "./props";
 
 // IDEA Types week, month, year
@@ -19,8 +17,6 @@ import { defaultProps, propTypes } from "./props";
 
 export const Input = (props) => {
   const { variantProps, mergeProps, mergeQuickProps } = useVariantMerger("Input", props);
-
-  const location = useLocation();
 
   const { 
     id,
