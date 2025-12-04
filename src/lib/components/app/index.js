b256a039d8d01943459343180fb6f7768cbb5e77
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-12-04 15:52:17 +0100

    remove AuthProvider (old code)

--- src/lib/components/app/index.js
+++ src/lib/components/app/index.js
@@ -3,5 +3,4 @@ export * from "./NavigationProvider";
 export * from "./LibConfigProvider";
 export * from "./Toaster";
 export * from "./Provider";
-// export * from "./AuthProvider";
 export * from "./ReduxProvider";
