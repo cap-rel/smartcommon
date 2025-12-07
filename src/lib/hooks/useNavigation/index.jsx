f374af951cc13759dabc79cd8f86f044745881a0
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-12-07 03:13:40 +0100

    useNavigation

--- src/lib/hooks/useNavigation/index.jsx
+++ src/lib/hooks/useNavigation/index.jsx
@@ -1,5 +1,44 @@
-import { useContext } from "react";
+import { useLocation, useNavigate, useParams, useSearchParams } from "react-router-dom";
+import { useGlobalStates } from "../export";
+import { isFunction, last } from "lodash";
+import { useEffect } from "react";
+import { log } from "lib/utils";
 
-import { NavigationContext } from "lib/components";
+export const useNavigation = ({ debug = false }) => {
+    const initialStates = { session: { history: [] } };
+    const gst = useGlobalStates({ initialStates });
 
-export const useNavigation = () => useContext(NavigationContext);
+    const history = gst.values?.history;
+
+    const location = useLocation();
+
+    useEffect(() => {
+        const { key, pathname, state } = location;
+        const lastLocation = last(history);
+
+        if (!lastLocation || lastLocation.key !== key) {
+            gst.set("history[]", location, "session");
+            
+            if (debug) {
+                log.location(`pathname = ${pathname}, state =`, state);
+            }
+        }
+    }, [location]);
+
+    const navigate = useNavigate();
+
+    const params = useParams();
+    const searchParams = useSearchParams();
+    
+    // ---------------------- return ----------------------
+
+    return  {
+        params,
+        searchParams,
+        now: location,
+        prev: history[history.length - 2],
+        to: navigate,
+        updateState: (state) => navigate(location.pathname, { state: isFunction(state) ? state(location.state) : state }),
+        replaceTo: (pathname) =>  navigate(pathname, { replace: true })
+    };
+};
