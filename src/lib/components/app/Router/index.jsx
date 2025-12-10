3ef162a1adcd11d70012f460a26158a1cac3fe3c
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-12-10 14:44:59 +0100

    Router

--- src/lib/components/app/Router/index.jsx
+++ src/lib/components/app/Router/index.jsx
@@ -1,11 +1,30 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { map } from "lodash";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-export const Router = ({ children }) => {
+import { throwTypeError } from "lib/utils";
+import { NavigationProvider } from "lib/components";
+
+export const Router = ({ routes = [] }) => {
+    throwTypeError({ value: routes, name: "routes", type: ["array"] });
+
+    const mapRoutes = (routes) => 
+        map(routes, ({ path, element, children = [] }, RI) => {
+            throwTypeError({ value: children, name: "Route children", type: ["array"] });
+
+            return (
+                <Route key={`route${RI}`} path={path} element={element}>
+                    {mapRoutes(children)}
+                </Route>
+            );
+        });
+
     return (
         <BrowserRouter>
-            <Routes>
-                {children}
-            </Routes>
+            <NavigationProvider>
+                <Routes>
+                    {mapRoutes(routes)}
+                </Routes>
+            </NavigationProvider>
         </BrowserRouter>
     );
 };
