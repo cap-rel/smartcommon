c57322ee9e5c6557d2f87248d2005f90bd7d6680
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-12-10 15:13:45 +0100

    export context from components

--- src/lib/components/app/index.js
+++ src/lib/components/app/index.js
@@ -1,8 +1,14 @@
 export * from "./I18nextProvider";
-export * from "./NavigationProvider";
-export * from "./LibConfigProvider";
 export * from "./Toaster";
 export * from "./Provider";
 export * from "./ReduxProvider";
 export * from "./Router";
+
+export * from "./LibConfigProvider";
+export * from "./LibConfigProvider/context";
+
+export * from "./NavigationProvider";
+export * from "./NavigationProvider/context";
+
 export * from "./ApiProvider";
+export * from "./ApiProvider/context";
