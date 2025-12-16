5831d502b39ea051394f5f10d1a95b89ef93cdee
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-12-16 17:17:59 +0100

    formDefaultValues

---
+++ src/lib/utils/maps/formDefaultValues.js
@@ -0,0 +1,44 @@
+const formDefaultValues = (props) => {
+    const { type, multiple } = props;
+
+    const defaultValues = {
+        boolean      : false,
+        check        : multiple ? [] : "",
+        select       : multiple ? [] : "",
+        array        : [],
+        varchar      : "",
+        email        : "",
+        password     : "",
+        phoneNumber  : "",
+        url          : "",
+        ip           : "",
+        // link         : "",
+        timestamp    : "",
+        date         : "",
+        datetime     : "",
+        time         : "",
+        int          : "",
+        float        : "",
+
+        // stock        : Input,
+        // reel         : Input,
+        // price        : Input,
+        // pricey       : Input,
+        // double       : Input,
+
+        rating       : 0,
+        range        : 0,
+        duration     : 0,
+        text         : "",
+        html         : "",
+        address      : "",
+        gpsPoints    : [null, null],
+        files        : multiple ? [] : { src: "", title: "", description: "", capture: false, createdAt: "", gpsPoints: [null, null] },
+        audios       : multiple ? [] : { src: "", title: "", description: "", capture: false, createdAt: "", gpsPoints: [null, null] },
+        videos       : multiple ? [] : { src: "", title: "", description: "", capture: false, createdAt: "", gpsPoints: [null, null] },
+        photos       : multiple ? [] : { src: "", title: "", description: "", capture: false, createdAt: "", gpsPoints: [null, null] },
+        signature    : { src: "", signer: "", signedAt: null, gpsPoints: [null, null] },
+        // icon         : IconSelect,
+        color        : ColorPicker,
+    }
+};
