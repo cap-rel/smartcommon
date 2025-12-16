19d8664d8b0923b8c406617786a6c57870be5718
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-12-16 13:44:16 +0100

    forgot children

--- src/lib/components/form/Form/index.jsx
+++ src/lib/components/form/Form/index.jsx
@@ -3,12 +3,12 @@ import { throwTypeError } from 'lib/utils';
 import { FormContext } from './context';
 
 export function Form(props) {
-  const { form } = props;
+  const { form, children } = props;
 
   throwTypeError({ value: form, name: "form", type: ["plain object"] });
 
   return (
-    <FormContext.Provider value={{ form, onSubmit }}>
+    <FormContext.Provider value={form}>
       {children}
     </FormContext.Provider>
   );
