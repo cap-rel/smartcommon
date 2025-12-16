02ecefe211790bc713e9670e50ea75daff14eb0a
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-12-16 11:28:16 +0100

    resolve useEffects bad system

--- src/lib/hooks/local/useEffects/index.jsx
+++ src/lib/hooks/local/useEffects/index.jsx
@@ -1,31 +1,49 @@
 import { useEffect, useRef } from "react";
 import { constant, forEach, isFunction, isPlainObject, mapValues, toArray } from "lodash";
 
-import { log } from "lib/utils";
+import { log, throwTypeError } from "lib/utils";
 
-export const useEffects = (effects) => {
-    if (!isPlainObject(effects)) {
-        throw new Error("effects must be a plain object.");
-    }
+// export const useEffects = (effects) => {
+//     if (!isPlainObject(effects)) {
+//         throw new Error("effects must be a plain object.");
+//     }
 
-    const activationsRef = useRef(mapValues(effects, constant(1)));
+//     const activationsRef = useRef(mapValues(effects, constant(1)));
 
-    forEach(effects, ({ deps, effect = () => {}, debug = false }, key) => {
-        if (isFunction(effect)) {
-            deps = toArray(deps);
+//     forEach(effects, ({ deps, effect = () => {}, debug = false }, key) => {
+//         if (isFunction(effect)) {
+//             deps = toArray(deps);
 
-            let activation = activationsRef.current[key];
+//             let activation = activationsRef.current[key];
 
-            useEffect(() => {
-                if (debug) {
-                    log.effect(`${key} (${activation}})`);
-                }
+//             useEffect(() => {
+//                 if (debug) {
+//                     log.effect(`${key} (${activation}})`);
+//                 }
 
-                activation += 1
-                effect();
-            }, deps);
+//                 activation += 1
+//                 effect();
+//             }, deps);
+//         }
+//     });
+
+//     return { activations: activationsRef.current };
+// };
+
+export const useEffect = (props = {}) => {
+    throwTypeError({ value: props, name: "props", type: ["plain object"] });
+
+    const { deps, fn = () => {}, debug } = props;
+
+    throwTypeError({ value: fn, name: "fn", type: ["plain object"] });
+
+    const depsArray = toArray(deps);
+
+    useEffect(() => {
+        if (debug) {
+            log.effect(`${key} (${activation}})`);
         }
-    });
 
-    return { activations: activationsRef.current };
+        fn();
+    }, depsArray);
 };
