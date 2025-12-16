fc7c5b26733625facc716c7c85fcf66e618f9500
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-12-16 17:36:49 +0100

    Try with not exporting from react-router-dom

--- src/lib/components/app/Routes/index.jsx
+++
@@ -1,4 +0,0 @@
-import { Routes as Rs, Route as R } from "react-router-dom";
-
-export const Routes = (props) => <Rs { ...props}/>;
-export const Route = (props) => <R { ...props}/>;
--- src/lib/components/app/export.js
+++ src/lib/components/app/export.js
@@ -1,4 +1,7 @@
 export * from "./Provider";
 export * from "./Toaster";
 export * from "./NavigationProvider";
-export * from "./Routes";
+
+import { Routes as Rs, Route as R } from "react-router-dom";
+
+export { Rs as Routes, R as Route };
