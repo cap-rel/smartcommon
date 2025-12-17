bbae4385e516bbff09bc9ef3a3ecee49024df0ba
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-12-17 11:19:18 +0100

    type to export some other dep components

--- src/lib/components/app/export.js
+++ src/lib/components/app/export.js
@@ -1,7 +1,3 @@
 export * from "./Provider";
 export * from "./Toaster";
 export * from "./NavigationProvider";
-
-import { Routes as Rs, Route as R, Outlet as O, Navigate as N } from "react-router-dom";
-
-export { Rs as Routes, R as Route, O as Outlet, N as Navigate };
--- src/lib/export.js
+++ src/lib/export.js
@@ -1,4 +1,17 @@
 export * from "./components/export";
 export * from "./hooks/export";
 export * from "./utils/export";
-export * from "./global-state/export";
+
+export * from "lodash";
+
+import { Routes as Rs, Route as R, Outlet as O, Navigate as N } from "react-router-dom";
+
+export { Rs as Routes, R as Route, O as Outlet, N as Navigate };
+
+import { Helmet as H } from "react-helmet";
+
+export { H as Head };
+
+import t from "react-hot-toast";
+
+export { t as toast };
