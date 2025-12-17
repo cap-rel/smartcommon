58d90eed9f9398f36e1a79f1fb7bb0261be19540
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-12-17 10:08:56 +0100

    export Outlet too

--- src/lib/components/app/export.js
+++ src/lib/components/app/export.js
@@ -2,6 +2,6 @@ export * from "./Provider";
 export * from "./Toaster";
 export * from "./NavigationProvider";
 
-import { Routes as Rs, Route as R } from "react-router-dom";
+import { Routes as Rs, Route as R, Outlet as O } from "react-router-dom";
 
-export { Rs as Routes, R as Route };
+export { Rs as Routes, R as Route, O as Outlet };
