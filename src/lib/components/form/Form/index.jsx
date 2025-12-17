eea576e1cb20ec732aa835afc0db1eb656858bad
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-12-17 10:31:56 +0100

    Forgot to put children

--- src/lib/components/form/Form/index.jsx
+++ src/lib/components/form/Form/index.jsx
@@ -3,7 +3,7 @@ import { throwTypeError } from 'lib/utils';
 import { FormContext } from './context';
 
 export function Form(props) {
-  const { form } = props;
+  const { form, children } = props;
 
   throwTypeError({ value: form, name: "form", type: ["plain object"] });
 
