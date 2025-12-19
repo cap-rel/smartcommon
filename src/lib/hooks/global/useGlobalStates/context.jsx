70ec980f6e4ac28270acc3d48ab0375740bf5d46
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-12-19 14:31:53 +0100

    dispatch can not return something

--- src/lib/hooks/global/useGlobalStates/context.jsx
+++ src/lib/hooks/global/useGlobalStates/context.jsx
@@ -42,7 +42,9 @@ export const useGlobalStatesContext = (props = {}) => {
     });
   }, []);
   
-  useEffect(() => dispatch(setGlobalStates(st.values)), [st.values]);
+  useEffect(() => {
+    dispatch(setGlobalStates(st.values))
+  }, [st.values]);
 
   // ---------------------- get ----------------------
 
