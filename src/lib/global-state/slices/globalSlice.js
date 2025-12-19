0b8540c98b3951d9dc6d570b5727c63af8f83923
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2025-12-19 14:51:39 +0100

    remove globalSlice function

--- src/lib/global-state/slices/globalSlice.js
+++ src/lib/global-state/slices/globalSlice.js
@@ -3,78 +3,78 @@ import { reduce, isArray } from "lodash";
 
 import { local, session } from "lib/utils";
 
-// ---------------------- parsePath (Parser) ----------------------
-const parsePath = (path) => {
-  const parts = [];
-  path.split(".").forEach(segment => {
-    const regex = /([\w-]+)|\[(\d*)\]/g;
-    let match;
-    while ((match = regex.exec(segment)) !== null) {
-      if (match[1]) parts.push(match[1]);
-      else if (match[2] === "") parts.push("__PUSH__");
-      else parts.push(match[2]);
-    }
-  });
-  return parts;
-};
+// // ---------------------- parsePath (Parser) ----------------------
+// const parsePath = (path) => {
+//   const parts = [];
+//   path.split(".").forEach(segment => {
+//     const regex = /([\w-]+)|\[(\d*)\]/g;
+//     let match;
+//     while ((match = regex.exec(segment)) !== null) {
+//       if (match[1]) parts.push(match[1]);
+//       else if (match[2] === "") parts.push("__PUSH__");
+//       else parts.push(match[2]);
+//     }
+//   });
+//   return parts;
+// };
 
-// ---------------------- set ----------------------
-const set = (acc, value, path) => {
-  const newState = { ...acc };
-  const parts = parsePath(path);
+// // ---------------------- set ----------------------
+// const set = (acc, value, path) => {
+//   const newState = { ...acc };
+//   const parts = parsePath(path);
 
-  let level = newState;
-  for (let i = 0; i < parts.length - 1; i++) {
-    const key = parts[i];
-    const nextKey = parts[i + 1];
-    const isNextPush = nextKey === "__PUSH__";
+//   let level = newState;
+//   for (let i = 0; i < parts.length - 1; i++) {
+//     const key = parts[i];
+//     const nextKey = parts[i + 1];
+//     const isNextPush = nextKey === "__PUSH__";
 
-    if (level[key] !== Object(level[key])) {
-      level[key] = /^\d+$/.test(nextKey) || isNextPush ? [] : {};
-    }
+//     if (level[key] !== Object(level[key])) {
+//       level[key] = /^\d+$/.test(nextKey) || isNextPush ? [] : {};
+//     }
 
-    if (!(key in level)) {
-      level[key] = /^\d+$/.test(nextKey) || isNextPush ? [] : {};
-    }
+//     if (!(key in level)) {
+//       level[key] = /^\d+$/.test(nextKey) || isNextPush ? [] : {};
+//     }
 
-    level = level[key];
-  }
+//     level = level[key];
+//   }
 
-  const lastKey = parts[parts.length - 1];
+//   const lastKey = parts[parts.length - 1];
 
-  const applyValue = (target, keyOrIndex) => {
-    const prevValue =
-      keyOrIndex === "__PUSH__"
-        ? target[target.length - 1]
-        : target[keyOrIndex];
-    const newValue = typeof value === "function" ? value(prevValue) : value;
+//   const applyValue = (target, keyOrIndex) => {
+//     const prevValue =
+//       keyOrIndex === "__PUSH__"
+//         ? target[target.length - 1]
+//         : target[keyOrIndex];
+//     const newValue = typeof value === "function" ? value(prevValue) : value;
 
-    if (keyOrIndex === "__PUSH__") {
-      target.push(newValue);
-    } else {
-      target[keyOrIndex] = newValue;
-    }
-  };
+//     if (keyOrIndex === "__PUSH__") {
+//       target.push(newValue);
+//     } else {
+//       target[keyOrIndex] = newValue;
+//     }
+//   };
 
-  if (lastKey === "__PUSH__") {
-    if (isArray(level)) {
-      level = level instanceof Object ? level : [];
-    }
-    applyValue(level, "__PUSH__");
-  } else {
-    applyValue(level, lastKey);
-  }
+//   if (lastKey === "__PUSH__") {
+//     if (isArray(level)) {
+//       level = level instanceof Object ? level : [];
+//     }
+//     applyValue(level, "__PUSH__");
+//   } else {
+//     applyValue(level, lastKey);
+//   }
 
-  return newState;
-};
+//   return newState;
+// };
 
-const storages = { ...local.get("global"), ...session.get("global") };
+// const storages = { ...local.get("global"), ...session.get("global") };
 
-const initialState = reduce(storages, set, {});
+// const initialState = reduce(storages, set, {});
 
 const globalSlice = createSlice({
   name: "global",
-  initialState,
+  initialState: {},
   reducers: {
     setGlobalStates(state, action) {
       return action.payload;
