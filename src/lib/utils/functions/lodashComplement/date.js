edbaa81365be52d5ace03eac4656359f6cb8ea5f
Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
2026-01-09 12:52:57 +0100

    Add timestamp converting functions

---
+++ src/lib/utils/functions/lodashComplement/date.js
@@ -0,0 +1,9 @@
+import { floor, isDate } from "lodash";
+
+export function toSTimestamp(date) {
+    return floor((isDate(date) ? date.getTime() : date) / 1000);
+}
+
+export function toMsTimestamp(date) {
+    return isDate(date) ? date.getTime() : (date * 1000);
+}
